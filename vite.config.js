4493a8c921b538b480eafb8e59e61442d00544ce
Riyu Pratama <riyurahmatpratama6@gmail.com>
2026-02-08 17:51:39 +0700

    setting custom domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/riyu-portofolio/',   // <-- tambahkan ini
+  base: '/',   // <-- tambahkan ini
   plugins: [
     vue(),
     vueDevTools(),
