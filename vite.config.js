ea0f8ba619cc5d1a12be570502fc65517805b29c
Riyu Pratama <riyurahmatpratama6@gmail.com>
2026-05-16 17:47:17 +0700

    Fix: update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',   // <-- tambahkan ini
+  base: '/riyu-portofolio/',   // <-- tambahkan ini
   plugins: [
     vue(),
     vueDevTools(),
